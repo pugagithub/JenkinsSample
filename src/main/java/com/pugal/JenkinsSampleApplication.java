cb812118b5eb21982cf7857e513bfe17d62a2d2f
pugal_git <pugal.sym@gmail.com>
2020-08-31 17:35:34 +0530

    Initial Commit

---
+++ src/main/java/com/pugal/JenkinsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.pugal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pugal/JenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pugal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
